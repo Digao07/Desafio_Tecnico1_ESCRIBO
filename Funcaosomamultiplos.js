da315f5e34708e871e5d63b11496b8af7fc0b95c
Rodrigo Barros <rodrigo.barros04@hotmail.com>
2023-11-22 18:29:30 -0300

    Create Funcaosomamultiplos.js

---
+++ Funcaosomamultiplos.js
@@ -0,0 +1,11 @@
+function somaMultiplosDeTresOuCinco(numero) {
+    let soma = 0;
+    for (let i = 3; i < numero; i++) {
+      if (i % 3 === 0 || i % 5 === 0) {
+        soma += i;
+      }
+    }
+    return soma;
+  }
+
+console.log(somaMultiplosDeTresOuCinco(20));
